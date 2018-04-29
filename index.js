039ad18cbee95eceb74ad1f5c46611b823f817e5
nicolasS-SP <nicolas@smartpark.fr>
2018-04-29 05:59:32 +0200

    merge database

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const t = [
 	}
 ];
 
-const app = require('express')();
+const express = require('express');
+const app = express();
 const co = require('co');
 const mongoClient = require("mongodb").MongoClient
 const port = process.env.PORT || 3002;
@@ -19,13 +20,17 @@ const port = process.env.PORT || 3002;
 co(function* () {
 	const db = (yield mongoClient.connect(process.env.MONGOLAB_URI)).db();
 
-  app.get('/', wrapAsync(function* (req, res, next) {
-  	
+  app.get('/test', wrapAsync(function* (req, res, next) {  	
 //  	yield db.collection('accounts').insert(t[1]);
 //  	const accounts = yield db.collection('accounts').find().toArray();
-
 	const el = yield db.collection('accounts').find().limit(1).
   }));
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.use('/signin', express.static('form'));
 
   app.listen(port, _ => console.log('App is listening on port ', port));
   
