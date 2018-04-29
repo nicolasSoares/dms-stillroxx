e1c80b648bc5652a4de5cf1d46196a570b647cff
1angly <langly@langly.fr>
2018-04-29 13:06:20 +0200

    i'm drunk lol

--- index.js
+++ index.js
@@ -51,19 +51,16 @@ co(function* () {
 
 	app.get('/auth/register', function (req, res) {
 		res.sendFile(path.join(__dirname + '/form/register.html'));
-
-/*
-	    var captcha = svgCaptcha.create();
-	//    req.session.captcha = captcha.text;
-	    res.type('svg');
-	    res.status(200).send(captcha.data);
-*/	    
-
 	});  
   
 /*
-	app.post('/signin', function (req, res) {
-		
+	app.post('/auth/register', function (req, res) {
+		var captcha = svgCaptcha.create();
+	//  req.session.captcha = captcha.text;
+	    res.type('svg');
+	    res.status(200).send(captcha.data);		
+
+		res.end(`<p>CAPTCHA VALID: ${ captcha.check(req, req.body[captchaFieldName]) }</p>`)
 	});  
 */
 
