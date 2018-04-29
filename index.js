22395019f069d5812f8f57c4e2000d365b098855
1angly <langly@langly.fr>
2018-04-29 06:32:59 +0200

    add form and it's work baby

--- index.js
+++ index.js
@@ -12,26 +12,35 @@ const t = [
 ];
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const co = require('co');
 const mongoClient = require("mongodb").MongoClient
 const port = process.env.PORT || 3002;
+const path = require('path');
 
 co(function* () {
 	const db = (yield mongoClient.connect(process.env.MONGOLAB_URI)).db();
-
+  app.use(bodyParser.urlencoded({ extended: true }));  
   app.get('/test', wrapAsync(function* (req, res, next) {  	
 //  	yield db.collection('accounts').insert(t[1]);
 //  	const accounts = yield db.collection('accounts').find().toArray();
-	const el = yield db.collection('accounts').find().limit(1).
+	const el = yield db.collection('accounts').find().limit(1)
   }));
 
   app.get('/', (req, res) => {
     res.send('Hello World!');
   });
 
-  app.use('/signin', express.static('form'));
-
+  app.get('/signin', function (req, res) {
+    res.sendFile(path.join(__dirname + '/form/index.html'));
+  });  
+  
+/*
+  app.post('/signin', function (req, res) {
+    res.render('./form/signin.html', { name: req.body.name });
+  });  
+ */   
   app.listen(port, _ => console.log('App is listening on port ', port));
   
 }).catch(err => {
