9135b5173346b54fb324dc1a066c25148deddbb3
nicolasS-SP <nicolas@smartpark.fr>
2018-05-01 10:15:38 +0200

    cron to reload crons

--- clock.js
+++ clock.js
@@ -1,18 +1,26 @@
 const CronJob = require('cron').CronJob;
 const bot = require('./worker.js');
 const co = require('co');
-co(function*() {
-	const db = yield require('./db.js')();
-	const crons = yield db.getCrons();
-	crons.forEach(cron => {
-		console.log('create cron: ', cron.crontime, cron.name);
-		var	job = new CronJob({
-		  cronTime: cron.crontime,
-		  context: {name: cron.name},
-		  onTick: bot.testcron,
-		  start: true,
-		  timeZone: "America/Los_Angeles"
+(function loadCrons() {
+	co(function*() {
+		const db = yield require('./db.js')();
+		const crons = yield db.getCrons();
+		crons.forEach(cron => {
+			console.log('create cron: ', cron.crontime, cron.name);
+			var	job = new CronJob({
+			  cronTime: cron.crontime,
+			  context: {name: cron.name},
+			  onTick: bot.testcron,
+			  start: true,
+			  timeZone: "America/Los_Angeles"
+			});
 		});
 	});
-});
+})();
 
+var	job = new CronJob({
+  cronTime: '00 * * * * *',
+  onTick: loadCrons,
+  start: true,
+  timeZone: "America/Los_Angeles"
+});
