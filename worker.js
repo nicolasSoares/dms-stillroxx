8eb0cfa6147684bd3812f273ba6026964f9c0dba
nicolasS-SP <nicolas@smartpark.fr>
2018-05-01 10:02:59 +0200

    say hi

--- worker.js
+++ worker.js
@@ -1,7 +1,5 @@
 module.exports = {
   testcron: function() {
-  	console.log('IM N DA WORKA !! FOR REAL !');
-  	console.log(this.name);
-  	console.log("ouout");
+  	console.log(`Hi, i'm $(this.name)`);
   }
 };
