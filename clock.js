974c2cdedd411c9b38530a63aad9980f0db871c6
nicolasS-SP <nicolas@smartpark.fr>
2018-05-01 09:29:19 +0200

    test

--- clock.js
+++ clock.js
@@ -1,13 +1,12 @@
 const CronJob = require('cron').CronJob;
 const bot = require('./worker.js');
 const co = require('co');
-var job;
 co(function*() {
 	const db = yield require('./db.js')();
 	const crons = yield db.getCrons();
 	crons.forEach(cron => {
 		console.log(cron);
-		job = new CronJob({
+		new CronJob({
 			cronTime: '12 * * * * *',
 			context: {name: 'mange'},
 			ontTick: bot.testcron,
@@ -16,3 +15,11 @@ co(function*() {
 		});
 	});
 });
+
+	var	job = new CronJob({
+			cronTime: '* * * * * *',
+			context: {name: 'mange'},
+			ontTick: bot.testcron,
+			start: true,
+			timeZone: "America/Los_Angeles"
+		});
