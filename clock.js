bbcd0aaa72e01bd85934325fd3497e124cc41c66
nicolasS-SP <nicolas@smartpark.fr>
2018-05-01 08:11:48 +0200

    test clock

--- clock.js
+++ clock.js
@@ -2,8 +2,8 @@ const CronJob = require('cron').CronJob;
 const bot = require('./worker.js');
 
 var job = new CronJob({
-  cronTime: "8 6,7,8,9,10,11 * * *", // everyday, 9:13, 11:13, 4:13, 8:13,
-  onTick: bot.testcron(),
+  cronTime: "13 6,7,8,9,10,11 * * *", // everyday, 9:13, 11:13, 4:13, 8:13,
+  onTick: bot.testcron,
   start: true,
   timeZone: "America/Los_Angeles"
 });
