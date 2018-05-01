96d84c1a638629fd2cd7e14edb3569cd8523c77d
nicolasS-SP <nicolas@smartpark.fr>
2018-05-01 09:47:24 +0200

    set cron for accounts

--- clock.js
+++ clock.js
@@ -1,25 +1,18 @@
 const CronJob = require('cron').CronJob;
 const bot = require('./worker.js');
 const co = require('co');
-/*co(function*() {
+co(function*() {
 	const db = yield require('./db.js')();
 	const crons = yield db.getCrons();
 	crons.forEach(cron => {
 		console.log(cron);
-		new CronJob({
-			cronTime: '12 * * * * *',
-			context: {name: 'mange'},
-			ontTick: bot.testcron,
-			start: true,
-			timeZone: "America/Los_Angeles"
+		var	job = new CronJob({
+		  cronTime: "00 * * * * *",
+		  context: {name:'jeje'},
+		  onTick: bot.testcron,
+		  start: true,
+		  timeZone: "America/Los_Angeles"
 		});
 	});
 });
-*/
-	var	job = new CronJob({
-  cronTime: "00 * * * * *", // everyday, 9:13, 11:13, 4:13, 8:13,
-  context: {name:'mange'},
-  onTick: bot.testcron,
-  start: true,
-  timeZone: "America/Los_Angeles"
-		});
+
